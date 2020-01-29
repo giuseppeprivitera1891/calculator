035df0f712831ab1a73315a3078e9d2ce2d77b8e
giuse <giuse@DESKTOP-9GI2GJ8.station>
2020-01-29 13:46:27 +0100

    Insert subtraction() method in Calculation class

--- calculator/src/calculator/Calculation.java
+++ calculator/src/calculator/Calculation.java
@@ -5,4 +5,8 @@ public class Calculation {
 	public static double sum(double a, double b) {
 		return a + b;
 	}
+	
+	public static double subtraction(double a, double b) {
+		return a - b;
+	}
 }
--- calculator/src/calculator/Main.java
+++ calculator/src/calculator/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println(Calculation.sum(5, 5));
-
+		System.out.println("The result of sum is: " + (int) Calculation.sum(5, 5));
+		System.out.println("the result of subtraction is: " + (int) Calculation.subtraction(7, 3));
 	}
 
 }
