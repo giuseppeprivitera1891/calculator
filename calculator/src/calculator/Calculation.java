5cc69410e059a6fef3a5cd09d8747127b86ab741
giuse <giuse@DESKTOP-9GI2GJ8.station>
2020-01-29 13:34:21 +0100

    New sum() method in Calculation Class and Main Class

---
+++ calculator/src/calculator/Calculation.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class Calculation {
+	
+	public static double sum(double a, double b) {
+		return a + b;
+	}
+}
---
+++ calculator/src/calculator/Main.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Calculation.sum(5, 5));
+
+	}
+
+}
